0f3fc47aeabc476cfc1fa6d80148e535fce4380d
Ubuntu <ubuntu@ip-172-31-46-239.us-west-2.compute.internal>
2013-08-07 22:18:57 +0000

    render the content of the index page

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var fs=require('fs');
 var app = express();
 app.use(express.logger());
+var getContent=function(FileName){
+    fs.readFile(FileName, function (err, data) {
+        if (err) throw err;
+        return data;
+    });
+}
+var buf = new Buffer(256);
+len = buf.write('\u00bd + \u00bc = \u00be', 0);
+console.log(len + " bytes: " + buf.toString('utf8', 0, len));
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(getContent('/index'));
 });
 
 var port = process.env.PORT || 5000;

456ae0a7c68ee683623c5c53293538d580de0d21
Ubuntu <ubuntu@ip-172-31-46-239.us-west-2.compute.internal>
2013-08-08 19:53:46 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,22 +1,36 @@
 var express = require('express');
-var fs=require('fs');
 var app = express();
+var fs = require ('fs');
+var fileName="index.html"
 app.use(express.logger());
-var getContent=function(FileName){
-    fs.readFile(FileName, function (err, data) {
-        if (err) throw err;
-        return data;
-    });
+var FileExist = function(thefileName){
+    return fs.existsSync(thefileName);
+
 }
-var buf = new Buffer(256);
-len = buf.write('\u00bd + \u00bc = \u00be', 0);
-console.log(len + " bytes: " + buf.toString('utf8', 0, len));
+
+var returnFileData = function(thefileName){
+    if (FileExist(thefileName)==true){
+        var theReturnBuff=fs.readFileSync(thefileName);
+        var buffer= new Buffer(theReturnBuff);
+        var data=buffer.toString('utf8',0,buffer.length);
+        console.log(data);
+        return data;
+    }
+    else{
+        console.log("the file doesn't exist");
+        return "the file doesn't exist";
+    }
+
+}
+
+var theFileData= returnFileData(fileName);
 
 app.get('/', function(request, response) {
-  response.send(getContent('/index'));
+    console.log(theFileData);
+    response.send(theFileData);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
